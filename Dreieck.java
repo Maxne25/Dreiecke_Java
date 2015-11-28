a74da404be823ddc6bc5009baf8038c6d4fd8a4a
Maxne25 <maxinell@web.de>
2015-11-28 13:36:44 +0100

    Update Dreieck.java

--- Dreieck.java
+++ Dreieck.java
@@ -3,5 +3,12 @@ public class Dreieck()
   Punkt ecke1 = new Punkt(0,0);
   Punkt ecke2 = new Punkt();
   Punkt ecke3 = new Punkt();
+  
+  public Dreieck new Dreieck(double x1, double y1, double x2, double y2)
+  {
+    
+    
+    
+  }
 
 }
